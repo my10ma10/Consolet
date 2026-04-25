dad15579e3df62666bba2dd4c1ad4fa0a9ae4c2f
eyevievv <sokol28.nik@gmail.com>
2026-04-25 17:45:27 +0300

    refactor: change scripts

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,4 +3,4 @@ mkdir coverage
 lcov --capture --directory . --output-file coverage/coverage.info
 lcov --remove coverage/coverage.info '*/external/*' '*/tests/*' -o coverage/coverage_filtered.info
 lcov --extract coverage/coverage_filtered.info '*/src/*' -o coverage/coverage_final.info
-genhtml coverage/coverage_final.info --output-directory coverage
+genhtml coverage/coverage_final.info --output-directory coverage/coverage-html
